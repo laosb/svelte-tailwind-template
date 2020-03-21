59db1c597a181708727306709c1e87cc86227c14
Shibo Lyu <laosb@lns.unihubs.com>
2020-03-21 22:39:09 +0800

    init template.
    Signed-off-by: Shibo Lyu <laosb@lns.unihubs.com>

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name
+</script>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
